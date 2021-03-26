a17c3320395605bff13573c6a84e74acba68fcb9
Doremberg <dorembergczar@gmail.com>
2021-03-26 11:52:27 -0600

    GifsApp

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html'
+})
+export class ResultadosComponent {
+
+  // Aquí se muestran los resultados en pantalla
+  get resultados(){
+    return this.gifsService.resultados;
+  }
+
+  constructor(private gifsService: GifsService){
+
+  }
+}
